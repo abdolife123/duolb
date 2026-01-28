9e05ea35c8c9ac1900bc703aea9738e25c170a0b
abdolife123 <abdolife4@gmail.com>
2026-01-28 20:39:32 +0100

    NBN

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
 import tailwindcss from '@tailwindcss/vite'
+import cloudflare from '@astrojs/cloudflare'
 
 export default defineConfig({
   site: 'https://duolb.com',
-  output: "static",
+
+  output: 'server', // REQUIRED for DB-driven posts
+  adapter: cloudflare(),
 
   integrations: [
     mdx(),
